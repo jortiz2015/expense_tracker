6a2fc48571302ca892a84d1defa2afbcad2e33cd
unknown <jose.antonio.ortiz.jr@gmail.com>
2021-09-14 11:04:16 -0600

    Re Organizing Components file structure

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,18 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+export default function Expenses(props) {
+
+    const expenseList = props.expenses.map(expense =>
+        <
+        ExpenseItem title = { expense.title }
+        amount = { expense.amount }
+        date = { expense.date } > < /ExpenseItem>
+    );
+
+    return ( <
+        Card className = "expenses" > { expenseList } <
+        /Card>
+    );
+}
